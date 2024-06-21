15d6ca311e3ef4b1edadf2e78210e31bc2038c6e
China-xbc <137941170+China-xb@users.noreply.github.com>
2024-06-21 12:04:42 +0800

    Update ipdz.py

--- ipdz.py
+++ ipdz.py
@@ -1,5 +1,24 @@
 import geoip2.database
+import requests
+import os
 
+def download_geolite_mmdb(file_url, save_path):
+    response = requests.get(file_url, stream=True)
+    if response.status_code == 200:
+        with open(save_path, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=1024):
+                f.write(chunk)
+    else:
+        print(f"Failed to download the file. Status code: {response.status_code}")
+
+# 使用P3TERX仓库中的最新GeoLite2-Country.mmdb文件的直接下载链接
+file_url = 'https://github.com/P3TERX/GeoLite.mmdb/releases/latest/download/GeoLite2-Country.mmdb'
+save_path = 'GeoLite2-Country.mmdb'  # 保存到当前脚本目录
+
+download_geolite_mmdb(file_url, save_path)
+
+# 下面是你的主脚本代码，现在你可以安全地假设GeoLite2-Country.mmdb已经下载并保存在当前目录
+# ...
 # 读取IPv4地址的文件
 def read_ips(filename):
     with open(filename, 'r') as file:
