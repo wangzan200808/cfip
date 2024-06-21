1b84e56b4197b02ff2c8b30523a18f8f5a6aa6e7
China-xbc <137941170+China-xb@users.noreply.github.com>
2024-06-21 17:55:12 +0800

    Update ipdz.py

--- ipdz.py
+++ ipdz.py
@@ -1,6 +1,8 @@
 import os
 import glob
 import geoip2.database
+import requests
+from bs4 import BeautifulSoup
 
 # 读取IPv4地址的文件
 def read_ips(filename):
@@ -11,18 +13,41 @@ def read_ips(filename):
         print(f"The file {filename} was not found.")
         return []
 
-# 使用geoip2获取IP的国家代码
+# 使用geoip2获取IP的国家代码，如果失败则使用备用方法
 def get_country_code(ip, reader):
     try:
         response = reader.country(ip)
         return response.country.iso_code
     except Exception as e:
         print(f"Error fetching data for IP {ip}: {e}")
-        return 'Unknown'
+        return get_location(ip)  # 使用备用方法获取国家代码
+
+# 使用whois.pconline.com.cn和ip-api.com获取IP的地理位置信息
+def get_location(ip):
+    try:
+        response = requests.get(f"http://whois.pconline.com.cn/ipJson.jsp?ip={ip}")
+        if response.status_code == 200:
+            data = response.json()
+            if 'err' not in data or data['err'] == "":
+                return data['proCode']  # 返回省代码作为国家代码的替代
+            else:
+                print(f"Error fetching location for IP {ip} using whois.pconline.com.cn: {data['err']}")
+    except Exception as e:
+        print(f"Error fetching location for IP {ip} using http://whois.pconline.com.cn/ipJson.jsp?: {e}")
+
+    try:
+        response = requests.get(f"http://ip-api.com/json/{ip}")
+        data = response.json()
+        if data['status'] == 'success':
+            return data['countryCode']
+    except Exception as e:
+        print(f"Error fetching location for IP {ip} using ip-api.com: {e}")
+
+    return 'Unknown'  # 如果两个API都失败，则返回Unknown
 
 # 保存IP到对应的国家代码文件，并去除重复的IP地址
 def save_ip_to_file(ip, country_code):
-    if ip and country_code in ['HK', 'JP']:  # 确保IP和国家代码有效且为HK或JP
+    if ip and country_code:  # 确保IP和国家代码有效
         filename = f'{country_code}.txt'
         # 确保文件存在，如果不存在则创建
         if not os.path.exists(filename):
@@ -33,6 +58,7 @@ def save_ip_to_file(ip, country_code):
                 # 如果IP地址不存在，则写入
                 with open(filename, 'a') as file:
                     file.write(ip + '\n')
+
 # 删除所有.txt文件，除了特定的文件
 def delete_existing_country_files(exceptions):
     for file in glob.glob('*.txt'):
