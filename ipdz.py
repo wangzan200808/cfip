2046438acdcab48d095ee4e8ede890632225915b
China-xbc <137941170+China-xb@users.noreply.github.com>
2024-06-21 14:55:10 +0800

    Update ipdz.py

--- ipdz.py
+++ ipdz.py
@@ -1,12 +1,15 @@
 import os
 import glob
-import re
 import geoip2.database
 
 # 读取IPv4地址的文件
 def read_ips(filename):
-    with open(filename, 'r') as file:
-        return [line.strip() for line in file if line.strip()]
+    try:
+        with open(filename, 'r') as file:
+            return [line.strip() for line in file if line.strip()]
+    except FileNotFoundError:
+        print(f"The file {filename} was not found.")
+        return []
 
 # 使用geoip2获取IP的国家代码
 def get_country_code(ip, reader):
@@ -16,35 +19,34 @@ def get_country_code(ip, reader):
     except Exception as e:
         print(f"Error fetching data for IP {ip}: {e}")
         return 'Unknown'
+
 # 保存IP到对应的国家代码文件，并去除重复的IP地址
 def save_ip_to_file(ip, country_code):
-    country_code = country_code or 'Unknown'
-    filename = f'{country_code}.txt'
-    
-    # 如果文件不存在，先创建它
-    if not os.path.exists(filename):
-        open(filename, 'a').close()
-    
-    # 确保不会重复写入相同的IP地址
-    if ip + '\n' not in open(filename, 'r').readlines():
-        with open(filename, 'a') as file:
-            file.write(ip + '\n')
-# 删除所有以国家代码命名的.txt文件，除了特定的文件
+    if ip and country_code in ['HK', 'JP']:  # 确保IP和国家代码有效且为HK或JP
+        filename = f'{country_code}.txt'
+        # 确保不会重复写入相同的IP地址
+        if ip + '\n' not in open(filename, 'r').readlines():
+            with open(filename, 'a') as file:
+                file.write(ip + '\n')
+
+# 删除所有.txt文件，除了特定的文件
 def delete_existing_country_files(exceptions):
-    # 遍历当前目录中的所有.txt文件
     for file in glob.glob('*.txt'):
-        # 检查文件是否在排除列表中
         if file not in exceptions:
-            os.remove(file)
+            try:
+                os.remove(file)
+            except OSError as e:
+                print(f"Error deleting file {file}: {e}")
+
 # 主函数
 def main():
     # 指定不删除的文件列表
     exceptions = ['requirements.txt', 'dns_result.txt', 'Fission_domain.txt', 'Fission_ip.txt']
 
-    # 删除所有现有的国家代码文件，除了指定的文件
+    # 删除所有现有的.txt文件，除了指定的文件
     delete_existing_country_files(exceptions)
 
-    # 指定GeoLite2数据库文件的路
+    # 指定GeoLite2数据库文件的路径
     database_path = 'GeoLite2-Country.mmdb'
     with geoip2.database.Reader(database_path) as reader:
         ips = read_ips('Fission_ip.txt')
